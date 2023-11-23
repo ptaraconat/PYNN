c1f503fdb0e18dd26354e7c46d5a2cfb3c780a8c
ptaraconat <104505251+ptaraconat@users.noreply.github.com>
2023-11-23 22:59:32 +0100

    Continue GM implementation not finished yet

--- gaussian_mixture_numerics.py
+++ gaussian_mixture_numerics.py
@@ -6,7 +6,7 @@ def general_gaussian_probability(x_vec,mu_vec,sigma_mat):
 	arr_tmp = x_vec - mu_vec
 	inv_sig = np.linalg.inv(sigma_mat)
 	det_sig = np.linalg.det(sigma_mat)
-	dim = len(x)
+	dim = len(x_vec)
 	
 	dot1 = np.matmul(inv_sig,arr_tmp).T
 	dot2 = np.matmul(dot1,arr_tmp)
@@ -18,36 +18,92 @@ def general_gaussian_probability(x_vec,mu_vec,sigma_mat):
 
 class GaussianMixture : 
 	
-	def __init__(self):
+	def __init__(self,n_class):
 		self.sigma_list = None 
 		self.mu_list = None 
-		self.n_classes = None 
-		self.class_proportions = None 
+		self.n_classes = n_class
+		self.class_proportions = None
+		
+	def initialise_attributes(self,x):
+		
+		split_list = np.array_split(x,self.n_classes)
+		self.update(split_list,np.size(x,0))
+		
+	def update(self,split_list,data_size): 
+		mu_list = []
+		sigma_list = []
+		proportion_list = []
+		for split in split_list : 
+			sigma = np.cov(split.T)
+			mu= np.mean(split,axis = 0)
+			proportion = np.size(split,0)/data_size
+			sigma_list.append(sigma)
+			mu_list.append(mu)
+			proportion_list.append(proportion)
+		self.mu_list = mu_list
+		self.sigma_list = sigma_list
+		self.class_proportions = proportion_list
 		
 	def utils_disp_shapes(self): 
 		for i in range(self.n_classes):
 			print('Class '+ str(i))
 			print('mu ::: ', np.shape(self.mu_list[i]))
 			print('sigma: ', np.shape(self.sigma_list[i]))
+			
+	def compute_law(self,x_array,mu, sigma): 
+		return np.array([general_gaussian_probability(x_array[i,:],mu,sigma) for i in range(np.size(x_array,0))])
+			
+	def predict_proba(self,input_data): 
+		probabilities = np.zeros((np.size(input_data,0),self.n_classes))
+		for k in range(self.n_classes) : 
+			arr_tmp = self.class_proportions[k]*self.compute_law(input_data,self.mu_list[k],self.sigma_list[k])
+			probabilities[:,k] = arr_tmp
+			probabilities[:,k] /= np.sum([self.class_proportions[j]*self.compute_law(input_data,self.mu_list[j],self.sigma_list[j]) for j in range(self.n_classes)])
+		#denominator = np.sum(self.class_proportions*probabilities,axis = 1)
+		#probabilities = np.divide(probabilities,np.expand_dims(denominator,axis = 1))
+		return probabilities
+	
+	def predict(self,input_data) : 
+		probabilities = self.predict_proba(input_data)
+		classes = np.argmax(probabilities,axis = 1)
+		print(classes)
+		return classes
+		
+	def predict_split(self,input_data):
+		 classes = self.predict(input_data)
+		 split_list = [input_data[np.argwhere(classes == i).squeeze(),:] for i in range(self.n_classes) ]
+		 return split_list
+		 
+	def fit(self,input_data,max_iter = 100): 
+		self.initialise_attributes(input_data)
+		data_size = np.size(input_data,0)
+		data_dim = np.size(input_data,1)
+		stop_criterion = False 
+		prev_mu = [np.zeros(data_dim) for i in range(self.n_classes)]
+		prev_sigma = [np.zeros((data_dim,data_dim)) for i in range(self.n_classes)]
+		n = 0
+		while not stop_criterion and (n < max_iter) : 
+			print(n)
+			split_list = self.predict_split(input_data)
+			self.update(split_list,data_size)
+			
+			prev_mu = self.mu_list
+			prev_sigma = self.sigma_list
+			
+			stop_criterion = False
+			
+			n = n + 1
+			
+			
+		
 	
 
 if __name__ == '__main__':
-	gm = GaussianMixture()
-	mu1 = np.array([2,4])
-	sigma1 = np.array([[2,0],[0,2]])
-	mu2 = np.array([4,8])
-	sigma2 = np.array([[1,0],[0,1]])
-	mu3 = np.array([8,16])
-	sigma3 = np.array([[1,0],[0,1]])
-	gm.n_classes = 3
-	gm.mu_list = [mu1,mu2,mu3]
-	gm.sigma_list = [sigma1, sigma2, sigma3]
-	gm.utils_disp_shapes()
+	data_vec = np.array([[2,4],[8,3],[1.5,1.5],[1.5,2.5],[1.4,3],[7.5,3.2],[8,4],[7.2,3.4]])
 	
-	data_vec = np.array([[2,4],[1,3],[4,8]])
-	x = data_vec[0,:]
+	gm = GaussianMixture(2)
 	
-	print(general_gaussian_probability(x,mu1,sigma1))
+	gm.fit(data_vec)
 	
 	x = np.linspace(0,10,100)
 	xv, yv = np.meshgrid(x,x)
@@ -56,10 +112,14 @@ if __name__ == '__main__':
 	print(proba.shape)
 	for i in range(100): 
 		for j in range(100):
-			proba[i,j] = general_gaussian_probability(np.array([xv[i,j],yv[i,j]]), mu1, sigma1)
+			proba[i,j] = general_gaussian_probability(np.array([xv[i,j],yv[i,j]]), gm.mu_list[0], gm.sigma_list[0] )
 	
 	plt.pcolormesh(xv,yv,proba)
+	plt.scatter(data_vec[:,0],data_vec[:,1],c=gm.predict(data_vec))
 	plt.show()
+
+
+	
 	
 	
 
